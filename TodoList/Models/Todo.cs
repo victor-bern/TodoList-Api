00c6dbd8e45e971bb82cb1fe77f15d8f30b01144
Victor Bernardes <dartinjs@gmail.com>
2022-10-13 14:06:42 -0300

    Adding todo model

---
+++ TodoList/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoList.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
